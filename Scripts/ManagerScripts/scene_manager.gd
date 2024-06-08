eebaa62fe03d184842d50eea121b230af6f71238
Tre Smith <tre.smith@blendinteractive.com>
2024-06-08 15:12:36 -0500

    adding scene transitions

--- Scripts/ManagerScripts/scene_manager.gd
+++ Scripts/ManagerScripts/scene_manager.gd
@@ -1,1 +1,30 @@
 extends Node
+
+enum SceneType {
+	Menu,
+	Level,
+	Shop
+}
+
+var num_levels = 5
+var current_level = 0
+var current_scene_type = SceneType.Menu
+
+var game_scene = "res://Scenes/Main.tscn"
+var title_scene = "res://Scenes/UI/main_menu.tscn"
+
+func restart():
+	current_level = 0
+	current_scene_type = SceneType.Menu
+	get_tree().change_scene_to_file(title_scene)
+
+func next_level():
+	if current_scene_type != SceneType.Level && current_level <= num_levels:
+		current_level += 1
+		current_scene_type = SceneType.Level
+		get_tree().change_scene_to_file(game_scene)
+	elif current_scene_type == SceneType.Level && current_level < num_levels:
+		current_scene_type = SceneType.Shop
+		get_tree().change_scene_to_file(game_scene)
+	else:
+		restart()
---
+++ Scripts/UI/next_level.gd
@@ -0,0 +1,6 @@
+extends Control
+
+signal next_level
+
+func _on_next_level_btn_pressed():
+	next_level.emit()

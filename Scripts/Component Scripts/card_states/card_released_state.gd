e49c2f6cae9b89933716331591d8e7cf0477088b
Austin57201 <austin57201@gmail.com>
2024-06-08 23:38:35 -0500

    Update card_released_state.gd

--- Scripts/Component Scripts/card_states/card_released_state.gd
+++ Scripts/Component Scripts/card_states/card_released_state.gd
@@ -19,5 +19,6 @@ func enter() -> void:
 func on_input(_event: InputEvent) -> void:
 	if not played:
 		return
+	await get_tree().create_timer(0.3).timeout
 	transition_requested.emit(self, CardState.State.BASE)
 
